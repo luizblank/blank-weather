b2cb6e2e4a82b29a09403520a0b5b80a3b9026db
luizblank <luizrosacardoso@gmail.com>
2024-04-11 20:51:32 -0300

    Added more security verifications on backend

--- backend/controllers/WeatherController.js
+++ backend/controllers/WeatherController.js
@@ -10,12 +10,22 @@ module.exports = class WeatherController
 
         const decrypted = CryptoJS.AES.decrypt(req.body.position, secret).toString(CryptoJS.enc.Utf8);
         const position = JSON.parse(decrypted)
+
+        const verifynumber = typeof position.latitude != 'number' || typeof position.longitude != 'number';
+        const verifynull = position.latitude == null || position.longitude == null;
+
+        if (verifynumber || verifynull)
+            return res.status(400).send({ message: "Client's position is not valid!"});
         
-        const weather = await axios.get(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${position.latitude},${position.longitude}?key=${process.env.KEY}`);
-
-        const weather_string = JSON.stringify(weather.data);
-        const encrypted = CryptoJS.AES.encrypt(weather_string, secret).toString();
-
-        return res.status(200).send({ message: 'API request was a success', weather: encrypted});
+        try {
+            const weather = await axios.get(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${position.latitude},${position.longitude}?key=${process.env.KEY}`);
+            
+            const weather_string = JSON.stringify(weather.data);
+            const encrypted = CryptoJS.AES.encrypt(weather_string, secret).toString();
+    
+            return res.status(200).send({ message: 'API request was a success.', weather: encrypted});
+        } catch {
+            return res.status(502).send({ message: 'The server experienced an error when trying to make the API request.'})
+        }
     }
 }
